715d1467f94b8ef04735b8eea865fe365c1da39c
davislr2 <davislr2@appstate.edu>
2023-01-28 01:35:54 -0500

    Finished the demo class

--- Demo.java
+++ Demo.java
@@ -6,7 +6,8 @@ public class Demo {
 	
 		Scanner kb = new Scanner(System.in);
 		boolean flag = true;
-		String shapeChoice;
+		String shapeChoice = "";
+		Shape shape;
 		while(flag){
 			System.out.println("What type of shape do you want the area of? (triangle or pentagon)");
 			shapeChoice = kb.nextLine();
@@ -16,12 +17,22 @@ public class Demo {
 			
 		}
 		//Declare a shape variable then set it to either triangle or pentagon then do the stuff.
-		if (shapeChoice.equals("triangle") {
-
+		if (shapeChoice.equals("triangle")) {
+			System.out.print("Choose a base length: ");
+			double base = kb.nextDouble();
+			System.out.print("Choose a height: ");
+			double height = kb.nextDouble();
+			shape = new Triangle(base, height);
+			System.out.println("Your area is . . . " + shape.area());
 		}
-		else if (shapeChoice.equals("pentagon") {
-	
-		}	
+		else if (shapeChoice.equals("pentagon")) {
+			System.out.print("Choose a side length: ");
+			double side = kb.nextDouble();
+			shape = new Pentagon(side);
+			System.out.println("Your area is . . . " + shape.area());
+		}
+
+			
 	}
 
 }
